051ac0dbe0c88116061e13aa8bfa41d5d6268425
Gwerr1002 <98936739+Gwerr1002@users.noreply.github.com>
2022-12-23 18:58:00 -0600

    ESP32
    Actualización de la esp32 solo eliminé algunas líneas innecesarias

--- IMU/marcha/main.py
+++ IMU/marcha/main.py
@@ -1,4 +1,5 @@
-from machine import Pin, I2C
+#1
+from machine import Pin, I2C, mem32
 import uasyncio
 from time import sleep_ms
 import bluetooth
@@ -6,16 +7,20 @@ from ble import BLE_send_sequence
 from imu import MPU6050
 
 data = ""
+LED = 21
 
 def config():
+    global LED
     i2c = I2C(1, sda=Pin(23), scl=Pin(22), freq=400000)
     imu = MPU6050(i2c)
+    led = Pin(LED,Pin.OUT)
     BLE = bluetooth.BLE()
     p = BLE_send_sequence(BLE)
     return imu, p
 
 async def adq(imu,p):
-    global data
+    global data, LED
+    GPIO_OUT_REG = 0x3FF44004
     while True:
         if p.is_connected():
             ax=imu.accel.x
@@ -26,21 +31,21 @@ async def adq(imu,p):
             gz=imu.gyro.z
             data += f"{ax} {ay} {az} {gx} {gy} {gz}" + ","
             #print(data)
+            mem32[GPIO_OUT_REG]^= (1<<LED)
         await uasyncio.sleep_ms(17)
 
 async def ble_send(p):
     global data
     while True:
         if p.is_connected():
-            print(f"sending: {data}")
+            #print(f"sending: {data}")
             p.send(data)
-            print("sended")
+            #print("sended")
             data = ""
-        await uasyncio.sleep_ms(100)
+        await uasyncio.sleep_ms(90)
 
 
 def main():
-    global data
     imu, p = config()
     loop = uasyncio.get_event_loop()
     loop.create_task(adq(imu,p))
